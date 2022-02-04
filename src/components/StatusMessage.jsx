ff2fe478ad11824ae2992c1a4c57aa6249c59647
ashishrathod95 <anusudha.1212@gmail.com>
2022-02-04 19:00:54 +0530

    StatusMessage.jsx File Added

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const StatusMessage = ({winner,current}) => {
+  
+  const noMovesLeft = current.board.every((ele) => ele !== null);
+  
+  return (
+    <h2>
+      {winner && `Winner is ${winner}`}
+      {!winner && !noMovesLeft && `Next Player is ${current.isXNext ? 'X' : 'O'}`}
+      {!winner && noMovesLeft && 'Game Tied'}
+    </h2>
+  );
+};
+
+export default StatusMessage;
